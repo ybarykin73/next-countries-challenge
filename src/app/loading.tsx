61f928f8a30d4f403b79ec3cdb75ab3d56ff73d1
YuraBarykin <yBarykin@gmail.com>
2024-04-24 18:13:58 +0400

    Добавлен скелетон на главную страницу

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,20 @@
+import Skeleton from "../components/ui/skeleton/Skeleton"
+
 const Loading = () => {
   return (
-    <div>loading...</div>
+
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(265px, 1fr))',
+        justifyContent: 'space-between',
+        gridGap: '40px'
+      }}
+    >
+      {
+        new Array(12).fill(null).map((item, index: number) => <Skeleton key={index} />)
+      }
+    </div>
   )
 }
 
--- src/server/api/countries/getAllCountries.ts
+++ src/server/api/countries/getAllCountries.ts
@@ -1,5 +1,4 @@
 export const getAllCountrues = async () => {
-
   const res = await fetch(process.env.URL + '/api/countrue')
   const data = await res.json()
 
